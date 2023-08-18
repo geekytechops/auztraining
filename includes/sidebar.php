11cbfa2c628f9d12f64a8ab189f1330db4f5d349
Surya <saikiran.m.v.s.s@gmail.com>
2023-08-18 16:15:46 +0530

    initial load

---
+++ includes/sidebar.php
@@ -0,0 +1,34 @@
+            <!-- ========== Left Sidebar Start ========== -->
+            <div class="vertical-menu">
+
+                <div data-simplebar class="h-100">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+                        <!-- Left Menu Start -->
+                        <ul class="metismenu list-unstyled" id="side-menu">
+                            <li class="menu-title">Forms</li>
+                            <li>
+                                <a href="dashboard.php" class="waves-effect">
+                                    <i class="mdi mdi-home-variant-outline"></i>
+                                    <span>Dashboard</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="student_enquiry.php" class="waves-effect">
+                                    <i class="fab fa-wpforms"></i>
+                                    <span>Student's Enquiry</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="enrolment.php" class="waves-effect">
+                                    <i class="fab fa-wpforms"></i>
+                                    <span>Enrolment</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- Sidebar -->
+                </div>
+            </div>
+            <!-- Left Sidebar End -->

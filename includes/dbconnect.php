ee69e004f01059239e09655f28b0a7d95247fe47
saikiranmvss <saikiran.m.v.s.s@gmail.com>
2026-04-22 08:47:01 +0530

    db changes

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,4 +1,4 @@
 <?php 
 $connection=mysqli_connect('localhost','root','','auztraining');
-// $connection= mysqli_connect("localhost","admin_auztraining","auztraining","admin_auztraining");
+// $connection= mysqli_connect("localhost","u593282393_enq_dash_new","U593282393_enq_dash_new","u593282393_enq_dash_new");
 include('mail_function.php');
